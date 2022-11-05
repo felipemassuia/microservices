e0fecb62be2160c135626e22a7030dc24e32246d
Felipe Massuia <massuia98.felipe@gmail.com>
2022-11-05 03:18:18 -0300

    Eureka Server, registration and Spring Gateway

--- hr-api-gateway/src/main/java/com/microservices/hrapigateway/HrApiGatewayApplication.java
+++ hr-api-gateway/src/main/java/com/microservices/hrapigateway/HrApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.microsservices.hrworker;
+package com.microservices.hrapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class HrWorkerApplication {
+public class HrApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HrWorkerApplication.class, args);
+		SpringApplication.run(HrApiGatewayApplication.class, args);
 	}
 
 }
